91d00bcc8250f34f7c3b429297370d77acf93bce
datpt11 <tiendat.phung11@gmail.com>
2024-08-23 15:00:25 +0700

    chore: refactor size

--- android/src/main/kotlin/dev/steenbakker/mobile_scanner/MobileScanner.kt
+++ android/src/main/kotlin/dev/steenbakker/mobile_scanner/MobileScanner.kt
@@ -346,8 +346,8 @@ class MobileScanner(
             val analysis = analysisBuilder.build().apply { setAnalyzer(executor, captureOutput) }
             videoCapture = VideoCapture.withOutput(Recorder.Builder()
                 .setExecutor(executor)
-                .setQualitySelector(QualitySelector.from(Quality.SD))
-                .setTargetVideoEncodingBitRate(10000)
+                .setQualitySelector(QualitySelector.from(Quality.HD))
+                .setTargetVideoEncodingBitRate(20000)
                 .build())
             try {
                 camera = cameraProvider?.bindToLifecycle(
